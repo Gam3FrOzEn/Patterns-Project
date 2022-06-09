886b55203afef7094be5d61704df562769f1bba0
Gam3FrOzEn <85313171+Gam3FrOzEn@users.noreply.github.com>
2022-06-09 21:56:41 +0300

    Added Singleton Pattern

---
+++ src/MainClass.java
@@ -0,0 +1,9 @@
+import entities.SantaClaus;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        SantaClaus santaClaus = SantaClaus.getInstance();
+        
+    }
+}
---
+++ src/entities/SantaClaus.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class SantaClaus {
+
+    private static SantaClaus instance;
+
+    private SantaClaus() {
+
+    }
+
+    public static SantaClaus getInstance() {
+        if (instance == null)
+            instance = new SantaClaus();
+
+        return instance;
+    }
+
+}
